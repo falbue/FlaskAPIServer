c58e179c01ce5cb700d66e50708276aee435a4a6
falbue <cyansair05@gmail.com>
2025-11-30 13:18:39 +0500

    Добавлена типизация в конфиг

--- FlaskAPIServer/config.py
+++ FlaskAPIServer/config.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-env_path = Path('.') / '.env'
-env_path = Path('.') / '.env'
+env_path = Path(".") / ".env"
+env_path = Path(".") / ".env"
 if env_path.exists():
     load_dotenv(dotenv_path=env_path)
 else:
@@ -22,9 +22,9 @@ SMTP_USER=
 SMTP_PASSWORD=
 FROM_EMAIL=
 """
-    with open(env_path, 'w', encoding='utf-8') as f:
+    with open(env_path, "w", encoding="utf-8") as f:
         f.write(env_template)
-    
+
     raise RuntimeError(
         "Файл .env не найден и был создан автоматически.\n"
         "Пожалуйста, настройте его перед запуском:\n"
@@ -35,15 +35,11 @@ FROM_EMAIL=
     )
 
 
-CONVERSIONS = {
-    'DEBUG': lambda x: x.lower() in ['true', '1']
-}
-
-for key, value in os.environ.items():
-    if key in CONVERSIONS:
-        globals()[key] = CONVERSIONS[key](os.getenv(key, value))
-    else:
-        globals()[key] = os.getenv(key)
+SECRET_KEY: str = os.getenv("SECRET_KEY", "FlaskAPIServer")
+DB_PATH: str = os.getenv("DB_PATH", "data/database.db")
+LOG_PATH: str = os.getenv("LOG_PATH", "data")
+DEBUG: bool = os.getenv("DEBUG", "False").lower() in ["true", "1"]
+JWT_LIFETIME: int = int(os.getenv("JWT_LIFETIME", "24"))
 
 db_dir = os.path.dirname(DB_PATH)
 if db_dir:
@@ -54,3 +50,9 @@ if logs:
     os.makedirs(logs, exist_ok=True)
 
 required_env_vars = ["SECRET_KEY", "DB_PATH", "LOG_PATH", "JWT_LIFETIME", "DEBUG"]
+
+for key, value in os.environ.items():
+    if key in required_env_vars:
+        globals()[key] = os.getenv(key)
+    else:
+        globals()[key] = value

8e7afc6534ebc5d19dfa528fb18a168a8b744dcb
falbue <cyansair05@gmail.com>
2025-11-30 16:31:14 +0500

    pep-8

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os
 import re
 from setuptools import setup, find_packages
 
+
 def get_version():
     version_file = os.path.join(os.path.dirname(__file__), "version.py")
     if os.path.exists(version_file):
@@ -12,29 +13,31 @@ def get_version():
                 return match.group(1)
     return "0.0.1.dev0"
 
+
 def parse_requirements(filename):
-    with open(filename, 'r') as f:
-        return [line.strip() for line in f if line.strip() and not line.startswith('#')]
+    with open(filename, "r") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
 
 setup(
-    name='FlaskAPIServer',
+    name="flaskapiserver",
     version=get_version(),
     packages=find_packages(where="."),
     include_package_data=True,
     package_data={
         "developer_application": ["*"],
     },
-    install_requires=parse_requirements('requirements.txt'),
-    description='Готовый сервер flask для быстрого использования API',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='falbue',
-    author_email='cyansair05@gmail.com',
-    url='https://github.com/falbue/FlaskAPIServer',
+    install_requires=parse_requirements("requirements.txt"),
+    description="Готовый сервер flask для быстрого использования API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="falbue",
+    author_email="cyansair05@gmail.com",
+    url="https://github.com/falbue/FlaskAPIServer",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
+    python_requires=">=3.10",
 )
